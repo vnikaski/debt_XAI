a9ba6a5c871ed5e9acecab131334a937be5a6471
vnika <vskibicka@gmail.com>
2022-04-04 09:14:33 +0200

    Data loading

--- data/load_data.py
+++ data/load_data.py
@@ -1,4 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-def load_data(datapath: str, descpath: str) -> pd.DataFrame:
-    pass
+
+def load_data(datapath: str, descpath: str, test_size: float, namevars=True):
+    df = pd.read_csv(datapath)
+    if namevars:
+        desc = pd.read_csv(descpath, sep=';')
+        df[desc['OPIS'].values] = df
+        df = df.drop(columns=desc['NAZWA'].values)
+    X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,1:], df.iloc[:,0], test_size=test_size, random_state=123, shuffle=True, stratify=df[df.columns[0]])
+    return X_train, y_train, X_test, y_test
--- train.py
+++ train.py
@@ -9,9 +9,11 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--prep', choices=['ROS', 'SMOTE', 'RUS', 'weighted', 'normalize', 'pca', 'svd'], nargs="+")
 parser.add_argument('classifier', choices=['lr', 'rf', 'gb'])
-parser.add_argument('--datapath', type=str, default='temat_3_dane.csv')
-parser.add_argument('--descpath', type=str, default='temat_3_opis_zmiennych.csv')
-parser.add_argument('--savepath', type=str, default=None)
+parser.add_argument('--dataPath', type=str, default='temat_3_dane.csv')
+parser.add_argument('--descPath', type=str, default='temat_3_opis_zmiennych.csv')
+parser.add_argument('--savePath', type=str, default=None)
+parser.add_argument('--leaveCodeNames', action='store_false')
+parser.add_argument('--testSize', type=float, default=0.3)
 
 args = parser.parse_args()
 
@@ -19,13 +21,12 @@ preprocesses = make_preprocessor(args.prep)
 model = make_model(args.classifier)
 
 pipe_steps = preprocesses.append(('model', model))
-
 pipe = Pipeline(pipe_steps)
 
-X, y, X_test, y_test = load_data(args.datapath, args.descpath)
+X, y, X_test, y_test = load_data(args.dataPath, args.descPath, args.testSize, args.leaveCodeNames)
 
 pipe.fit(X, y)
 
 # saving the model
-if args.savepath is not None:
+if args.savePath is not None:
     pass
