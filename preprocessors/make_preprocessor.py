2e4a7c68b2a4e480c553895f21418a34e1ae78d1
vnika <vskibicka@gmail.com>
2022-04-06 21:20:35 +0200

    normalizer (uncertain), pca, svd

--- preprocessors/make_preprocessor.py
+++ preprocessors/make_preprocessor.py
@@ -1,9 +1,11 @@
 from typing import List
 from imblearn.over_sampling import RandomOverSampler, SMOTE
 from imblearn.under_sampling import RandomUnderSampler
+from sklearn.preprocessing import Normalizer
+from sklearn.decomposition import PCA, TruncatedSVD
 
 
-def make_preprocessor(processes: List[str], os_rate: float, us_rate: float, cur_ratio: float) -> List[tuple]:
+def make_preprocessor(processes: List[str], os_rate: float, us_rate: float, cur_ratio: float, n_components: int) -> List[tuple]:
     """
     :param processes: list of processes from args
     :param os_rate: rate of oversmapling (1 = None, 2 = twice as many)
@@ -25,5 +27,11 @@ def make_preprocessor(processes: List[str], os_rate: float, us_rate: float, cur_
         cur_ratio = cur_ratio * os_rate
     if 'RUS' in processes:
         preps.append(('RUS', RandomUnderSampler(sampling_strategy=cur_ratio * us_rate, random_state=0)))
+    if 'normalize' in processes: # not sure if normalizer would work in the pipeline
+        preps.append(('Normalizer', Normalizer(norm='l2')))
+    if 'pca' in processes:
+        preps.append(('PCA', PCA(n_components=n_components, random_state=0)))
+    if 'svd' in processes:
+        preps.append(('SVD', TruncatedSVD(n_components=n_components, random_state=0)))
 
     return preps
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from preprocessors.make_preprocessor import make_preprocessor
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--prep', choices=['ROS', 'SMOTE', 'RUS', 'weighted', 'normalize', 'pca', 'svd'], nargs="+")
+parser.add_argument('--prep', choices=['ROS', 'SMOTE', 'RUS', 'normalize', 'pca', 'svd'], nargs="+")
 parser.add_argument('classifier', choices=['lr', 'rf', 'gb'])
 parser.add_argument('--dataPath', type=str, default='temat_3_dane.csv')
 parser.add_argument('--descPath', type=str, default='temat_3_opis_zmiennych.csv')
@@ -16,11 +16,12 @@ parser.add_argument('--leaveCodeNames', action='store_false')
 parser.add_argument('--testSize', type=float, default=0.1)
 parser.add_argument('--osRate', type=float, default=1) # 1 = no oversampling, 2 = twice as many, etc
 parser.add_argument('--usRate', type=float, default=1) # 1 = no undersampling, 2 = half of neg samples, etc
+parser.add_argument('--nComponents', type=int, default=50)
 
 args = parser.parse_args()
 
 X, y, X_test, y_test = load_data(args.dataPath, args.descPath, args.testSize, args.leaveCodeNames)
-preprocesses = make_preprocessor(args.prep, args.osRate, args.usRate, y.sum()/(len(y)-y.sum()))
+preprocesses = make_preprocessor(args.prep, args.osRate, args.usRate, y.sum()/(len(y)-y.sum()), args.nComponents)
 model = make_model(args.classifier)
 
 pipe_steps = preprocesses.append(('model', model))
