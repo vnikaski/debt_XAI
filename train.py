d15174ed2fdd221a467821d1a9054667aacea4fe
vnika <vskibicka@gmail.com>
2022-04-06 21:39:33 +0200

    models

--- train.py
+++ train.py
@@ -17,12 +17,26 @@ parser.add_argument('--testSize', type=float, default=0.1)
 parser.add_argument('--osRate', type=float, default=1) # 1 = no oversampling, 2 = twice as many, etc
 parser.add_argument('--usRate', type=float, default=1) # 1 = no undersampling, 2 = half of neg samples, etc
 parser.add_argument('--nComponents', type=int, default=50)
+parser.add_argument('--learningRate', type=float, default=0.001) # good default LR for DL, not sure about less complex models
+parser.add_argument('--nEstimators', type=float, default=100)
+parser.add_argument('--maxDepth', type=float, default=None)
+parser.add_argument('--classWeight', type=str, choices=['balanced, balanced_subsample'], default=None)
 
 args = parser.parse_args()
 
 X, y, X_test, y_test = load_data(args.dataPath, args.descPath, args.testSize, args.leaveCodeNames)
-preprocesses = make_preprocessor(args.prep, args.osRate, args.usRate, y.sum()/(len(y)-y.sum()), args.nComponents)
-model = make_model(args.classifier)
+
+preprocesses = make_preprocessor(processes=args.prep,
+                                 os_rate=args.osRate,
+                                 us_rate=args.usRate,
+                                 cur_ratio=y.sum()/(len(y)-y.sum()),
+                                 n_components=args.nComponents)
+
+model = make_model(model_type=args.classifier,
+                   learning_rate=args.learningRate,
+                   n_estimatiors=args.nEstimators,
+                   max_depth=args.maxDepth,
+                   class_weight=args.classWeight)
 
 pipe_steps = preprocesses.append(('model', model))
 pipe = Pipeline(pipe_steps)
